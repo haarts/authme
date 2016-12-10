1699d27e5a865ec1da186f0d3fe7fa5d01b17d59
Harm Aarts <harmaarts@gmail.com>
2016-12-11 00:08:17 +0100

    schema: initial definition
    Includes users and sessions.

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `users` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `username` VARCHAR NOT NULL,
+    `encrypted_password` VARCHAR NOT NULL,
+    `salt` VARCHAR NOT NULL,
+    `email` VARCHAR NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `sessions` (
+	`session` VARCHAR NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
+)
